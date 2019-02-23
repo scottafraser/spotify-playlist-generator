a400915253da60c5b12b64cad0aaa641bd1e56b6
Scott Fraser <scottfraserdev@gmail.com>
2019-02-23 12:44:09 -0800

    fix store aggain

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";0.
+import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 
 export default function configureStore(intialState) {
