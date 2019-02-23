8bda79a2eb4b625077777700fcf817a0fc0fbd7f
Scott Fraser <scottfraserdev@gmail.com>
2019-02-23 12:02:49 -0800

    remove redux tools

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import thunk from "redux-thunk";0.
 import rootReducer from "../reducers";
 
 export default function configureStore(intialState) {
   return createStore(
     rootReducer,
     intialState,
-    applyMiddleware(thunk) + window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    //  + window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //   window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 }
